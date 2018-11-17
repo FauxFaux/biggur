dab4af9cd5975586d7c31e652e1561316ee4c61c
Chris West (Faux) <git@goeswhere.com>
2018-11-17 16:30:23 +0000

    extract module

---
+++ src/cache.rs
@@ -0,0 +1,95 @@
+use std::thread;
+use std::time::Duration;
+
+use failure::err_msg;
+use failure::Error;
+use failure::ResultExt;
+use reqwest::header::AUTHORIZATION;
+use rusqlite::types::ToSql;
+use serde_json::Value;
+
+use super::Instant;
+
+pub struct Cache {
+    pub db: rusqlite::Connection,
+    pub client: reqwest::Client,
+    pub client_id: String,
+}
+
+impl Cache {
+    pub fn fetch(&self, url: &str, cache_secs: i64) -> Result<Value, Error> {
+        let now = now();
+
+        if let Some(cached_at) = self
+            .db
+            .prepare_cached("select max(occurred) from raw where url=?")?
+            .query_row(&[url], |row| row.get::<_, Option<Instant>>(0))?
+        {
+            info!("{:?}: exists in cache, from {:?}", url, cached_at);
+            if now.signed_duration_since(cached_at).num_seconds() < cache_secs {
+                return Ok(self
+                    .db
+                    .prepare_cached("select returned from raw where occurred=? and url=?")?
+                    .query_row(&[&cached_at as &ToSql, &url], |row| row.get(0))?);
+            } else {
+                trace!("{:?}: ...but was too old", url)
+            }
+        }
+
+        let body = self.try_fetch_body(url)?;
+
+        let mut write_raw = self
+            .db
+            .prepare_cached("insert into raw (occurred, url, returned) values (?,?,?)")?;
+        write_raw.insert(&[&now as &ToSql, &url, &body])?;
+
+        Ok(body)
+    }
+
+    fn try_fetch_body(&self, url: &str) -> Result<Value, Error> {
+        for fetch in 0..4 {
+            match self.actually_fetch_body(url) {
+                Ok(val) => return Ok(val),
+                Err(e) => {
+                    warn!("fetch {} failed, will re-try. {:?}", fetch, e);
+                }
+            }
+
+            thread::sleep(Duration::from_secs(15));
+        }
+
+        self.actually_fetch_body(url)
+    }
+
+    fn actually_fetch_body(&self, url: &str) -> Result<Value, Error> {
+        info!("{:?}: fetching", url);
+
+        let body: Value = self
+            .client
+            .get(url)
+            .header(AUTHORIZATION, format!("Client-ID {}", self.client_id))
+            .send()?
+            .json()?;
+
+        trace!("returned: {:?}", body);
+
+        Ok(unpack_response(&body)
+            .with_context(|_| format_err!("unpacking {:?}, which returned {:?}", url, body))?
+            .to_owned())
+    }
+}
+
+fn unpack_response(body: &Value) -> Result<&Value, Error> {
+    let body = body.as_object().ok_or(err_msg("root wasn't object"))?;
+    ensure!(
+        body.get("success")
+            .and_then(|success| success.as_bool())
+            .unwrap_or(false),
+        "request wasn't success"
+    );
+    Ok(body.get("data").ok_or(err_msg("data absent"))?)
+}
+
+fn now() -> Instant {
+    chrono::Utc::now()
+}
--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,26 @@ extern crate log;
 #[macro_use]
 extern crate serde_json;
 
+mod cache;
 mod whole;
 
 use std::collections::HashSet;
 use std::env;
-use std::thread;
 use std::time::Duration;
 
 use cast::u64;
 use failure::err_msg;
 use failure::Error;
 use failure::ResultExt;
-use reqwest::header::AUTHORIZATION;
-use rusqlite::types::ToSql;
 use serde_json::Value;
 
 type Instant = chrono::DateTime<chrono::Utc>;
 type JsonObj = serde_json::Map<String, Value>;
 
-struct Cache {
-    db: rusqlite::Connection,
-    client: reqwest::Client,
-    client_id: String,
-}
-
 fn main() -> Result<(), Error> {
     pretty_env_logger::init();
 
-    let cache = Cache {
+    let cache = cache::Cache {
         db: rusqlite::Connection::open("biggur.db")?,
         client: reqwest::ClientBuilder::new()
             .timeout(Duration::from_secs(15))
@@ -50,7 +42,7 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
-fn load_expanded(cache: &Cache, gallery: &str) -> Result<Vec<(JsonObj, Vec<Value>)>, Error> {
+fn load_expanded(cache: &cache::Cache, gallery: &str) -> Result<Vec<(JsonObj, Vec<Value>)>, Error> {
     let mut albums = Vec::with_capacity(300);
     for page in 0..=5 {
         let url = format!(
@@ -90,7 +82,7 @@ fn load_expanded(cache: &Cache, gallery: &str) -> Result<Vec<(JsonObj, Vec<Value
     Ok(expanded)
 }
 
-fn expand_images(cache: &Cache, album: &JsonObj, id: &str) -> Result<Vec<Value>, Error> {
+fn expand_images(cache: &cache::Cache, album: &JsonObj, id: &str) -> Result<Vec<Value>, Error> {
     if let Some(images) = album.get("images").and_then(|images| images.as_array()) {
         let images_count = album
             .get("images_count")
@@ -117,82 +109,4 @@ fn expand_images(cache: &Cache, album: &JsonObj, id: &str) -> Result<Vec<Value>,
     } else {
         Ok(vec![Value::Object(album.to_owned())])
     }
-}
-
-impl Cache {
-    fn fetch(&self, url: &str, cache_secs: i64) -> Result<Value, Error> {
-        let now = now();
-
-        if let Some(cached_at) = self
-            .db
-            .prepare_cached("select max(occurred) from raw where url=?")?
-            .query_row(&[url], |row| row.get::<_, Option<Instant>>(0))?
-        {
-            info!("{:?}: exists in cache, from {:?}", url, cached_at);
-            if now.signed_duration_since(cached_at).num_seconds() < cache_secs {
-                return Ok(self
-                    .db
-                    .prepare_cached("select returned from raw where occurred=? and url=?")?
-                    .query_row(&[&cached_at as &ToSql, &url], |row| row.get(0))?);
-            } else {
-                trace!("{:?}: ...but was too old", url)
-            }
-        }
-
-        let body = self.try_fetch_body(url)?;
-
-        let mut write_raw = self
-            .db
-            .prepare_cached("insert into raw (occurred, url, returned) values (?,?,?)")?;
-        write_raw.insert(&[&now as &ToSql, &url, &body])?;
-
-        Ok(body)
-    }
-
-    fn try_fetch_body(&self, url: &str) -> Result<Value, Error> {
-        for fetch in 0..4 {
-            match self.actually_fetch_body(url) {
-                Ok(val) => return Ok(val),
-                Err(e) => {
-                    warn!("fetch {} failed, will re-try. {:?}", fetch, e);
-                }
-            }
-
-            thread::sleep(Duration::from_secs(15));
-        }
-
-        self.actually_fetch_body(url)
-    }
-
-    fn actually_fetch_body(&self, url: &str) -> Result<Value, Error> {
-        info!("{:?}: fetching", url);
-
-        let body: Value = self
-            .client
-            .get(url)
-            .header(AUTHORIZATION, format!("Client-ID {}", self.client_id))
-            .send()?
-            .json()?;
-
-        trace!("returned: {:?}", body);
-
-        Ok(unpack_response(&body)
-            .with_context(|_| format_err!("unpacking {:?}, which returned {:?}", url, body))?
-            .to_owned())
-    }
-}
-
-fn unpack_response(body: &Value) -> Result<&Value, Error> {
-    let body = body.as_object().ok_or(err_msg("root wasn't object"))?;
-    ensure!(
-        body.get("success")
-            .and_then(|success| success.as_bool())
-            .unwrap_or(false),
-        "request wasn't success"
-    );
-    Ok(body.get("data").ok_or(err_msg("data absent"))?)
-}
-
-fn now() -> Instant {
-    chrono::Utc::now()
 }
